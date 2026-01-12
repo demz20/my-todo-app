f692ce5d2e5a9be252ba53fcbfcbf6420848b551
Judems Daub <judems.daub@filamsoftware.com>
2026-01-13 05:25:44 +0800

    fix: comment out allowDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowDevOrigins: true
+  // allowDevOrigins: true
 };
 
 export default nextConfig;
